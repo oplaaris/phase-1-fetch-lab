187071f17c59764030fba0c55c50c07d12626931
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-04 22:34:43 +0300

    was unable to complete

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+  
