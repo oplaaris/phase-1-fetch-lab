5d5940ee56846c47e2e8b954d2107d812dbf647b
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-05 09:14:47 +0300

    lab completed

--- index.js
+++ index.js
@@ -16,8 +16,29 @@ document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
 
+let books = [];
+const book = {
+  name: 'A Game of Thrones',
+  // ...
+};
 
+console.log(book.name);
 fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
   .then((json) => console.log(json));
   
+
+ // The 5th book in the series
+ console.log(`The 5th book in the series: ${books[4].name}`);
+
+ // The total number of pages of all the books
+ const totalPages = books.reduce((acc, book) => acc + book.numberOfPages, 0);
+ console.log(`The total number of pages of all the books: ${totalPages}`);
+
+ // The 1031st character in the series
+ fetch("https://anapioficeandfire.com/api/characters/1031")
+   .then((resp) => resp.json())
+   .then((character) => {
+     console.log(`The 1031st character in the series: ${character.name}`);
+   });
+
